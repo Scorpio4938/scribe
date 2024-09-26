02e917adafec4f1568575a90895abe50ebf7f2c3
Scorpio493.8 <Scorpio493.8@gmail.com>
2024-09-26 23:19:40 +0800

    add the hyphenate function

--- lib/src/main/java/com/scorpio4938/scribe/service/utils/ScribeStringProcessor.java
+++ lib/src/main/java/com/scorpio4938/scribe/service/utils/ScribeStringProcessor.java
@@ -1,7 +1,8 @@
 package com.scorpio4938.scribe.service.utils;
 
-import java.io.IOException;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class ScribeStringProcessor {
 
@@ -14,5 +15,36 @@ public class ScribeStringProcessor {
      */
     public static String joinList(List<String> list) {
         return String.join(" ", list);
+    }
+
+    /**
+     * Join two string using hyphen.
+     *
+     * @return A hyphen-separated string.
+     * @since v0.1.0
+     */
+    public static String hyphenate(String a, String b) {
+        return String.join("-", a, b);
+    }
+
+    /**
+     * Join the list using hyphen.
+     *
+     * @return A hyphen-separated string list.
+     * @since v0.1.0
+     */
+    public static List<String> hyphenate(List<String> list) {
+        return List.of(String.join("-", list));
+    }
+
+    /**
+     * Join two list of string using hyphen.
+     *
+     * @return A hyphen-separated string list.
+     * @since v0.1.0
+     */
+    public static List<String> hyphenate(List<String> a, List<String> b) {
+        return List.of(Stream.concat(a.stream(), b.stream())
+                .collect(Collectors.joining("-")));
     }
 }
--- lib/src/test/java/com/scorpio4938/scribe/service/utils/ScribeStringProcessorTest.java
+++ lib/src/test/java/com/scorpio4938/scribe/service/utils/ScribeStringProcessorTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.util.List;
 
 public class ScribeStringProcessorTest {
     private final String txtStringPath = "src/test/resources/exampleString.txt";
@@ -12,6 +13,16 @@ public class ScribeStringProcessorTest {
     @Test
     void joinListTest() throws IOException {
         ScribeFileReader reader = new ScribeFileReader();
-        Assertions.assertEquals("Alice Jay", ScribeStringProcessor.joinList(reader.txtStringArrayListReader(txtStringPath)));
+        Assertions.assertEquals("Alice Jay", ScribeStringProcessor.joinList(reader.txtStringArrayListReader(txtStringPath)), "ScribeStringProcessor.joinList should join two list of string using black");
+    }
+
+    @Test
+    void hyphenateTest() throws IOException {
+        ScribeFileReader reader = new ScribeFileReader();
+        List<String> read = reader.txtStringArrayListReader(txtStringPath);
+        Assertions.assertEquals("Alice-Jay", ScribeStringProcessor.hyphenate(read.get(0), read.get(1)), "ScribeStringProcessor.hyphenate() should join two list of string using hyphen");
+        Assertions.assertEquals(List.of("Alice-Jay"), ScribeStringProcessor.hyphenate(read), "ScribeStringProcessor.hyphenate() should join two list of string using hyphen");
+        Assertions.assertEquals(List.of("Alice-Jay"), ScribeStringProcessor.hyphenate(List.of(read.get(0)), List.of(read.get(1))), "ScribeStringProcessor.hyphenate() should join two list of string using hyphen");
+
     }
 }
