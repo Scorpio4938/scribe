343d8c682eae7f0d64960c9d0ac454526889ca4e
Scorpio493.8 <Scorpio493.8@gmail.com>
2024-10-26 21:19:48 +0800

    major test for llm client

--- lib/src/test/java/com/scorpio4938/scribe/api/llm/LLMApiClientTest.java
+++ lib/src/test/java/com/scorpio4938/scribe/api/llm/LLMApiClientTest.java
@@ -1,4 +1,23 @@
 package com.scorpio4938.scribe.api.llm;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class LLMApiClientTest {
+    private final boolean call = false; // Whether to use llm
+
+    String LLMCall() throws Exception {
+        if (call) {
+            LLMApiClient llm = new LLMApiClient(new Providers().getProvider("DEEPSEEK"), null);
+            return llm.callLLM("deepseek-chat", "give me a english name");
+        }
+        return null;
+    }
+
+    @Test
+    void callLLMTest() throws Exception {
+        String result = this.LLMCall();
+//        Assertions.assertNotNull(result);
+        System.out.print("llm result: " + result + "\n");
+    }
 }
